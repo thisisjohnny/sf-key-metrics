eea8ea62908788acfb1f39f321909f2f872d798b
John Scardino <jscardino@jscardino-ltm1.internal.salesforce.com>
2023-12-21 17:40:02 -0500

    Additional configuration settings added and initial localization support framework

--- force-app/main/default/lwc/keyMetric/keyMetric.js
+++ force-app/main/default/lwc/keyMetric/keyMetric.js
@@ -1,4 +1,6 @@
 import { LightningElement, api } from 'lwc';
+import LOCALE from '@salesforce/i18n/locale';
+import CURRENCY from '@salesforce/i18n/currency';
 
 export default class KeyMetric extends LightningElement {
     @api metric;
--- force-app/main/default/lwc/keyMetrics/keyMetrics.js
+++ force-app/main/default/lwc/keyMetrics/keyMetrics.js
@@ -1,8 +1,10 @@
 import { LightningElement, api, wire } from 'lwc';
-import getMetadataValues from "@salesforce/apex/KeyMetricsController.getMetadataValues";
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getMetadataValues from '@salesforce/apex/KeyMetricsController.getMetadataValues';
 
 export default class KeyMetrics extends LightningElement {
     @api customMetadataLabel;
+    @api recordId;
 
     metrics = [];
 
@@ -11,25 +13,31 @@ export default class KeyMetrics extends LightningElement {
         if (data) {
             if (data.Card_Format__c == 2) {
                 this.metrics = [
-                    { id: 1, apiName: data.Metric_1_API_Name__c},
-                    { id: 2, apiName: data.Metric_2_API_Name__c}
+                    { id: 1, recordId: this.recordId, objectApiName: data.Object_API_Name__c, apiName: data.Metric_1_API_Name__c, label: data.Metric_1_Label__c, format: data.Metric_1_Format__c },
+                    { id: 2, recordId: this.recordId, objectApiName: data.Object_API_Name__c, apiName: data.Metric_2_API_Name__c, label: data.Metric_2_Label__c, format: data.Metric_2_Format__c}
                 ];
             } else if (data.Card_Format__c == 3) {
                 this.metrics = [
-                    { id: 1, apiName: data.Metric_1_API_Name__c},
-                    { id: 2, apiName: data.Metric_2_API_Name__c},
-                    { id: 3, apiName: data.Metric_3_API_Name__c}
+                    { id: 1, recordId: this.recordId, objectApiName: data.Object_API_Name__c, apiName: data.Metric_1_API_Name__c, label: data.Metric_1_Label__c, format: data.Metric_1_Format__c},
+                    { id: 2, recordId: this.recordId, objectApiName: data.Object_API_Name__c, apiName: data.Metric_2_API_Name__c, label: data.Metric_2_Label__c, format: data.Metric_2_Format__c},
+                    { id: 3, recordId: this.recordId, objectApiName: data.Object_API_Name__c, apiName: data.Metric_3_API_Name__c, label: data.Metric_3_Label__c, format: data.Metric_3_Format__c}
                 ];
             } else if (data.Card_Format__c == 4) {
                 this.metrics = [
-                    { id: 1, apiName: data.Metric_1_API_Name__c},
-                    { id: 2, apiName: data.Metric_2_API_Name__c},
-                    { id: 3, apiName: data.Metric_3_API_Name__c},
-                    { id: 4, apiName: data.Metric_4_API_Name__c}
+                    { id: 1, recordId: this.recordId, objectApiName: data.Object_API_Name__c, apiName: data.Metric_1_API_Name__c, label: data.Metric_1_Label__c, format: data.Metric_1_Format__c},
+                    { id: 2, recordId: this.recordId, objectApiName: data.Object_API_Name__c, apiName: data.Metric_2_API_Name__c, label: data.Metric_2_Label__c, format: data.Metric_2_Format__c},
+                    { id: 3, recordId: this.recordId, objectApiName: data.Object_API_Name__c, apiName: data.Metric_3_API_Name__c, label: data.Metric_3_Label__c, format: data.Metric_3_Format__c},
+                    { id: 4, recordId: this.recordId, objectApiName: data.Object_API_Name__c, apiName: data.Metric_4_API_Name__c, label: data.Metric_4_Label__c, format: data.Metric_4_Format__c}
                 ];
             }
         } else if (error) {
-            // jawn
+            const errorToastEvent = new ShowToastEvent({
+                title: "Oops! Something went wrong.",
+                message: "Ensure you have configured your custom metadata type and supplied the correct label in the LWC settings on this page.",
+                mode: "dismissible",
+                variant: "error"
+            });
+            this.dispatchEvent(errorToastEvent);
         }
     }
 }
