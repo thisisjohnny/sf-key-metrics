4bccb1ee2415c8a07c08d4b8c50e912e1357351d
John Scardino <jscardino@jscardino-ltm1.internal.salesforce.com>
2023-12-21 16:00:14 -0500

    Apex added to pull metadata and updated js to send to child component

--- force-app/main/default/lwc/keyMetrics/keyMetrics.js
+++ force-app/main/default/lwc/keyMetrics/keyMetrics.js
@@ -1,19 +1,35 @@
 import { LightningElement, api, wire } from 'lwc';
+import getMetadataValues from "@salesforce/apex/KeyMetricsController.getMetadataValues";
 
 export default class KeyMetrics extends LightningElement {
-    @api color;
-    examples = [
-        {
-            id: 1,
-            apiName: "My_Custom_Field_1__c"
-        },
-        {
-            id: 2,
-            apiName: "My_Custom_Field_2__c"
-        },
-        {
-            id: 3,
-            apiName: "My_Custom_Field_3__c"
+    @api customMetadataLabel;
+
+    metrics = [];
+
+    @wire(getMetadataValues, { customMetadataLabel: "$customMetadataLabel" })
+    metadataValues({ error, data }) {
+        if (data) {
+            if (data.Card_Format__c == 2) {
+                this.metrics = [
+                    { id: 1, apiName: data.Metric_1_API_Name__c},
+                    { id: 2, apiName: data.Metric_2_API_Name__c}
+                ];
+            } else if (data.Card_Format__c == 3) {
+                this.metrics = [
+                    { id: 1, apiName: data.Metric_1_API_Name__c},
+                    { id: 2, apiName: data.Metric_2_API_Name__c},
+                    { id: 3, apiName: data.Metric_3_API_Name__c}
+                ];
+            } else if (data.Card_Format__c == 4) {
+                this.metrics = [
+                    { id: 1, apiName: data.Metric_1_API_Name__c},
+                    { id: 2, apiName: data.Metric_2_API_Name__c},
+                    { id: 3, apiName: data.Metric_3_API_Name__c},
+                    { id: 4, apiName: data.Metric_4_API_Name__c}
+                ];
+            }
+        } else if (error) {
+            // jawn
         }
-    ];
+    }
 }
